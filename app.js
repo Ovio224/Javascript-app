6c1881a9cd3d6d7f133dd39a0cc78e1630fb5c24
Ovi# <covidiu97@gmail.com>
2018-09-15 16:59:03 +0200

    Made it look prettier

--- app.js
+++ app.js
@@ -1,118 +1,119 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('registrar');
-  const input = form.querySelector('input');
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("registrar");
+  const input = form.querySelector("input");
 
-  const mainDiv = document.querySelector('.main');
-  const ul = document.getElementById('invitedList');
+  const mainDiv = document.querySelector(".main");
+  const ul = document.getElementById("invitedList");
 
-  const div = document.createElement('div');
-  const filterLabel = document.createElement('label');
-  const filterCheckbox = document.createElement('input');
+  const div = document.createElement("div");
+  const filterLabel = document.createElement("label");
+  const filterCheckbox = document.createElement("input");
 
   filterLabel.textContent = "Hide those who haven't responded";
-  filterCheckbox.type = 'checkbox';
+  filterCheckbox.type = "checkbox";
   div.appendChild(filterLabel);
   div.appendChild(filterCheckbox);
   mainDiv.insertBefore(div, ul);
 
-  filterCheckbox.addEventListener('change', (e) =>{
+  filterCheckbox.addEventListener("change", e => {
     const isChecked = e.target.checked;
     const lis = ul.children;
     if (isChecked) {
       for (let i = 0; i < lis.length; i++) {
         let li = lis[i];
-        if (li.className === 'responded'){
-          li.style.display = '';
+        if (li.className === "responded") {
+          li.style.display = "";
         } else {
-          li.style.display = 'none';
+          li.style.display = "none";
         }
       }
     } else {
-        for (let i = 1; i < lis.length; i++) {
+      for (let i = 1; i < lis.length; i++) {
         let li = lis[i];
-        li.style.display = '';
+        li.style.display = "";
       }
     }
-  })
-
-
+  });
 
   function createLI(text) {
-    function createElement (elementName, property, value) {
+    function createElement(elementName, property, value) {
       const element = document.createElement(elementName);
       element[property] = value;
       return element;
     }
-    function appendToLi (elementName, property, value){
+    function appendToLi(elementName, property, value) {
       const element = createElement(elementName, property, value);
       li.appendChild(element);
       return element;
     }
-    const li = document.createElement('li');
-    appendToLi('span', 'textContent', text);
-    appendToLi('label', 'textContent', 'Confirmed').appendChild(createElement('input', 'type', 'checkbox'));
-    appendToLi('button', 'textContent', 'Edit');
-    appendToLi('button', 'textContent', 'Remove');
+    const li = document.createElement("li");
+    appendToLi("span", "textContent", text);
+    appendToLi("label", "textContent", "Confirmed").appendChild(
+      createElement("input", "type", "checkbox")
+    );
+    appendToLi("button", "textContent", "Edit");
+    appendToLi("button", "textContent", "Remove");
     return li;
   }
 
-  form.addEventListener('submit', (e) =>{
-      e.preventDefault();
-      const text = input.value;
-      input.value = '';
-        const li = createLI(text);
-      ul.appendChild(li);
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    const text = input.value;
+    input.value = "";
+    const li = createLI(text);
+    ul.appendChild(li);
 
-      if(text === '') {
-        alert("Hey! You gotta type a real name!")
-        ul.removeChild(li);
-      }
-      if (text === document.getElementbyTagName('span').textContent) {
-        alert("You're already coming to the party!")
-      }
-  })
+    if (text === "") {
+      alert("Hey! You gotta type a real name!");
+      ul.removeChild(li);
+    }
+    if (text === document.getElementbyTagName("span").textContent) {
+      alert("You're already coming to the party!");
+    }
+  });
 
-  ul.addEventListener('change', (e) => {
+  ul.addEventListener("change", e => {
     const checkbox = event.target;
     const checked = checkbox.checked;
     const listItem = checkbox.parentNode.parentNode;
     if (checked) {
-      listItem.className = 'responded';
+      listItem.className = "responded";
     } else {
-      listItem.className = '';
+      listItem.className = "";
     }
   });
 
-  ul.addEventListener('click', (e) => {
-    if (e.target.tagName === 'BUTTON') {
+  ul.addEventListener("click", e => {
+    if (e.target.tagName === "BUTTON") {
       const button = e.target;
       const li = e.target.parentNode;
       const ul = li.parentNode;
       const action = button.textContent;
-      const nameActions =
-    {
-        Remove: () => { ul.removeChild(li); },
+      const nameActions = {
+        Remove: () => {
+          ul.removeChild(li);
+        },
         Edit: () => {
-        const span = li.firstElementChild;
-        const input = document.createElement('input');
-        input.type = 'text';
-        input.value = span.textContent;
-        li.insertBefore(input, span);
-        li.removeChild(span);
-        button.textContent = 'Save';
-      },
+          const span = li.firstElementChild;
+          const input = document.createElement("input");
+          input.type = "text";
+          input.value = span.textContent;
+          li.insertBefore(input, span);
+          li.removeChild(span);
+          button.textContent = "Save";
+        },
         Save: () => {
-        const input = li.firstElementChild;
-        const span = document.createElement('span');
-        span.textContent = input.value;
-        li.insertBefore(span, input);
-        li.removeChild(input);
-        button.textContent = 'Edit';
-      }
-    };
+          const input = li.firstElementChild;
+          const span = document.createElement("span");
+          span.textContent = input.value;
+          li.insertBefore(span, input);
+          li.removeChild(input);
+          button.textContent = "Edit";
+        }
+      };
 
       // select and run action for button's name
       nameActions[action]();
-  }
+    }
   });
 });
